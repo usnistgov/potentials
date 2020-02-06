d2db9d57ffbca868f8bfb01f6f5cbcd3f3d36368
Lucas Hale <lucas.hale@nist.gov>
2020-02-06 14:32:38 -0500

    version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def getreadme():
         return readme_file.read()
    
 setup(name = 'potentials',
-      version = '0.1.0',
+      version = '0.1.1',
       description = 'API database tools for accessing the NIST Interatomic Potentials Repository: explore and download interatomic potentials and computed properties.',
       long_description = getreadme(),
       classifiers=[
