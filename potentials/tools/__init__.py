8df593aab46bce8915bbf7e8f407a37c769d42f1
Lucas Hale <lucas.hale@nist.gov>
2022-02-16 14:58:42 -0500

    uber_open_rmode now inherited from DataModelDict

--- potentials/tools/__init__.py
+++ potentials/tools/__init__.py
@@ -1,8 +1,9 @@
-from datamodelbase.tools import aslist, iaslist, screen_input
+# coding: utf-8
+from yabadaba.tools import aslist, iaslist, screen_input
+from DataModelDict import uber_open_rmode
 from .atomic_info import *
 from .atomic_info import __all__ as atomic_info_all
 from .parse_authors import parse_authors
-from .uber_open_rmode import uber_open_rmode
 from .numderivative import numderivative
 
 __all__ = ['aslist', 'iaslist', 'screen_input', 'uber_open_rmode', 'parse_authors',
